20d0a9552ea4b3a205d8c5f179bc94025ff18e9b
Duong1009 <duongb2014559@student.ctu.edu.vn>
2024-02-01 19:42:51 +0700

    cap nhat

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const contactsRouter = require("./app/routes/contact.route");
 app.use(cors());
 app.use(express.json());
 
+
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to contact book application."});
 });
